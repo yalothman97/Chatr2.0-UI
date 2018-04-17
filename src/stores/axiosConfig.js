0563246595246288f55d1a6c481c5d6420494d16
Octowl <alsaff1987@gmail.com>
2018-04-17 17:18:25 +0300

    Dummy welcome screen

--- src/stores/axiosConfig.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-axios.defaults.baseURL = 'http://192.168.100.54';
-
-export default function setAuthToken() {
-  if(localStorage.getItem('token')) axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
-  else delete axios.defaults.headers.common['Authorization'];
-};
