3752c44c37f4cc5ce2e30150eaf34ee0d2198798
Octowl <alsaff1987@gmail.com>
2018-04-17 17:18:25 +0300

    Dummy welcome screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
+import {observer} from 'mobx-react';
 
 // Components
 import NavBar from './components/NavBar';
 import LogoutModal from './components/LogoutModal';
 import LoginModal from './components/LoginModal';
 import SignupModal from './components/SignupModal';
+import Welcome from './components/Welcome';
 import Footer from './components/Footer';
 
 function App(props) {
@@ -12,6 +15,7 @@ function App(props) {
   return (
     <div className="content-wrapper">
       <NavBar authStore={authStore} channelStore={channelStore}/>
+      {!authStore.isLoggedIn && <Welcome />}
       <LogoutModal authStore={authStore}/>
       <LoginModal authStore={authStore}/>
       <SignupModal authStore={authStore}/>
---
+++ src/components/Welcome.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+
+class Welcome extends Component {
+  render() {
+    return (
+      <header className="masthead d-flex">
+        <div className="container text-center my-auto z-1">
+          <h1 className="mb-1">Stylish Portfolio</h1>
+          <h3 className="mb-5">
+            <em>You're gonna need to login to see the messages</em>
+          </h3>
+          <button className="btn btn-primary btn-lg"
+            data-toggle="modal"
+            data-target="#loginModal">Login</button>
+        </div>
+        <div className="overlay z-0"></div>
+      </header>
+    );
+  }
+}
+
+export default Welcome;
