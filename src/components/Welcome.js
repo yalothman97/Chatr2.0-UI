6d31db796f6634a911d865adf0c2f96fb751c656
Octowl <alsaff1987@gmail.com>
2018-04-17 17:46:55 +0300

    Random welcome messages

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,12 +1,26 @@
 import React, {Component} from 'react';
 
+import getRandomMessage from '../data/welcomeMessages';
 
 class Welcome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {welcomeMessage: ""}
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.setState({welcomeMessage: getRandomMessage()}), 2500)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
     return (
       <header className="masthead d-flex">
         <div className="container text-center my-auto z-1">
-          <h1 className="mb-1">Stylish Portfolio</h1>
+          <h1 className="mb-1">{this.state.welcomeMessage}</h1>
           <h3 className="mb-5">
             <em>You're gonna need to login to see the messages</em>
           </h3>
